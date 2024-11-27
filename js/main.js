84cf7bdaedb2149a53086cc13c15249bc93d37d5
longlin <developerlin123@gmail.com>
2024-11-27 10:22:14 +0100

    rm os

--- js/main.js
+++ js/main.js
@@ -1,14 +1,8 @@
- AOS.init({
- 	duration: 800,
- 	easing: 'slide',
- 	once: true
- });
+
 
 jQuery(document).ready(function($) {
 
-	"use strict";
-
-	
+	"use strict";	
 
 	var siteMenuClone = function() {
 
